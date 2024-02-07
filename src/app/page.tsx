48929168b4fe212eabc14221ab3a93d3f4a12c04
utxo detective <harrison@beckerich.org>
2024-02-07 14:47:19 -0600

    color flower

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center p-24">
+		<main className="flex min-h-screen flex-col items-center justify-center p-24 bg-black">
 			<Image
 				className="relative"
 				src="/next.svg"
