c852f2ec9bc88fe795b5b4c5766a7751ed2bdee6
utxo detective <harrison@beckerich.org>
2024-02-07 14:36:28 -0600

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center p-24">
+			<Image
+				className="relative"
+				src="/next.svg"
+				alt="Next.js Logo"
+				width={180}
+				height={37}
+				priority
+			/>
+		</main>
+	)
+}
