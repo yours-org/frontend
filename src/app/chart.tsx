f6ec102123ce23b8cd28392e3d366f8ccc16f35b
NlockTime <159491556+NlockTime@users.noreply.github.com>
2024-02-17 09:19:26 -0800

    Update chart.tsx

--- src/app/chart.tsx
+++ src/app/chart.tsx
@@ -1,272 +1,266 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Chart from 'react-apexcharts';
-import formatNumber from '@/utils/format-number';
-import useExchangeRate from '@/utils/hooks/useExchangeRate';
-import classNames from 'classnames';
+'use client'
 
-const TABS = ['1h', '4h', '6h', '12h', '1D'];
+import { createChart, ColorType } from 'lightweight-charts'
+// @ts-ignore
+import React, { useEffect, useRef } from 'react'
+import formatNumber from '@/utils/format-number'
+import useExchangeRate from '@/utils/hooks/useExchangeRate'
+import classNames from 'classnames'
+
+const TABS = ['1h', '4h', '6h', '12h', '1D']
 
 function groupData(data, interval) {
-    const groupedData = {};
-    const intervalMilliseconds = {
-        '10m': 10 * 60 * 1000,
-        '1h': 60 * 60 * 1000,
-        '4h': 60 * 60 * 1000 * 4,
-        '6h': 60 * 60 * 1000 * 6,
-        '12h': 60 * 60 * 1000 * 12,
-        '1D': 24 * 60 * 60 * 1000
-    };
+	const groupedData = {}
+	const intervalMilliseconds = {
+		'10m': 10 * 60 * 1000,
+		'1h': 60 * 60 * 1000,
+		'4h': 60 * 60 * 1000 * 4,
+		'6h': 60 * 60 * 1000 * 6,
+		'12h': 60 * 60 * 1000 * 12,
+		'1D': 24 * 60 * 60 * 1000
+	}
 
-    data.forEach((item) => {
-        const date = new Date(item.time * 1000);
-        const roundedDate = new Date(
-            Math.floor(date.getTime() / intervalMilliseconds[interval]) * intervalMilliseconds[interval]
-        );
+	data.forEach((item) => {
+		const date = new Date(item.time * 1000)
+		// Round down the date to the nearest interval
+		const roundedDate = new Date(
+			Math.floor(date.getTime() / intervalMilliseconds[interval]) * intervalMilliseconds[interval]
+		)
 
-        const key = roundedDate.toISOString();
-        if (!groupedData[key]) {
-            groupedData[key] = [];
-        }
-        groupedData[key].push(item);
-    });
+		// Use the rounded date as a key for grouping
+		const key = roundedDate.toISOString()
+		if (!groupedData[key]) {
+			groupedData[key] = []
+		}
+		groupedData[key].push(item)
+	})
 
-    return groupedData;
+	return groupedData
 }
 
-export default function ChartComponent(props) {
-    const { data, unlockData, height } = props;
-    const { exchangeRate } = useExchangeRate();
-    const [selectedTab, setSelectedTab] = useState('1D'); // Set default time interval to '1D'
-    const [chartType, setChartType] = useState('area'); // Set default chart type to 'area'
-    const chartRef = useRef(null);
-    const [showPieChart, setShowPieChart] = useState(false);
+export default function Chart(props: {
+	height: number
+	data: any
+	unlockData: any
+	mempoolData: any
+}) {
+	const { data, unlockData, mempoolData, height } = props
+	const { exchangeRate } = useExchangeRate()
+	const [selectedTab, setSelectedTab] = React.useState('1D')
 
-    useEffect(() => {
-        const groups = groupData(data, selectedTab);
+	const { dayAgoTvl, tvl, percentChange } = React.useMemo(() => {
+		if (!data?.length || !unlockData?.length) {
+			return null
+		}
+		const lastLock = data.slice(-1)[0]
+		const lastUnlock = unlockData
+			.filter((e) => parseInt(e.height) <= parseInt(lastLock.height))
+			.slice(-1)[0]
 
-        const lockedData = Object.keys(groups).map((key) => {
-            const totalLocked = groups[key].reduce((acc, curr) => acc + parseInt(curr.sum), 0);
-            return {
-                x: new Date(key).getTime(),
-                y: totalLocked
-            };
-        });
+		const lastLockHeight = parseInt(lastLock.height, 10)
 
-        if (chartRef.current) {
-            chartRef.current.chart.updateSeries([{ name: 'Locked', data: lockedData }]);
-        }
-    }, [data, height, selectedTab]);
+		const dayAgoLock = data.filter((e) => parseInt(e.height) < lastLockHeight - 144).slice(-1)[0]
+		const dayAgoUnlock = unlockData
+			.filter((e) => parseInt(e.height) <= parseInt(dayAgoLock.height))
+			.slice(-1)[0]
 
-    const tvl = React.useMemo(() => {
-        if (!data?.length || !unlockData?.length) {
-            return null;
-        }
-        const lastLock = data.slice(-1)[0];
-        const lastUnlock = unlockData.filter((e) => parseInt(e.height) <= parseInt(lastLock.height)).slice(-1)[0];
-        return (parseInt(lastLock.sum) - parseInt(lastUnlock.sum)) / 1e8;
-    }, [data, unlockData]);
+		const mempoolSats = mempoolData?.sats || 0
+		const tvl = (parseInt(lastLock.sum) - parseInt(lastUnlock.sum)) / 1e8
+		const dayAgoTvl = (parseInt(dayAgoLock.sum) - parseInt(dayAgoUnlock.sum)) / 1e8
 
-    const renderTab = (e) => {
-        return (
-            <div
-                className={classNames(
-                    'flex justify-center bg-[#17191E] cursor-pointer text-xs rounded-lg p-2 font-semibold shadow-md',
-                    {
-                        ['text-white']: selectedTab === e,
-                        ['text-[#D0D5DD]']: selectedTab !== e
-                    }
-                )}
-                key={e}
-                onClick={() => setSelectedTab(e)}
-            >
-                {e}
-            </div>
-        );
-    };
+		console.log({
+			lastLock,
+			lastUnlock,
+			dayAgoLock,
+			dayAgoUnlock,
+			diff: tvl - dayAgoTvl,
+			mempool: mempoolSats
+		})
 
-    const handleChartTypeChange = (type) => {
-        setChartType(type);
-        if (type === 'line') {
-            setSelectedTab('4h'); // Set default time interval to 4 hours for Line Chart
-        }
-    };
+		const percentChange = ((tvl - dayAgoTvl) / dayAgoTvl) * 100
 
-    const togglePieChart = () => {
-        setShowPieChart(!showPieChart);
-    };
+		return { tvl, percentChange, dayAgoTvl }
+	}, [data, unlockData, mempoolData])
 
-    return (
-        <div className="h-full w-full relative">
-            <div className="md:absolute l-0 t-0 z-10 flex flex-col px-4 gap-2">
-                <p className="text-sm font-semibold text-white">Locked Coins</p>
-                <div className="bg-[#17191E] rounded-lg p-4 flex justify-between gap-8">
-                    <div className="flex gap-2 items-center">
-                        <img src="/bsv.svg" className="h-4 w-4" alt="BSV" />
-                        <p className="text-2xl text-white whitespace-nowrap">{formatNumber(tvl.toFixed(2))}</p>
-                    </div>
-                    <div>
-                        <p className="text-2xl text-white whitespace-nowrap">
-                            ${formatNumber((tvl * exchangeRate).toFixed(2))}
-                        </p>
-                    </div>
-                </div>
-                <div className="gap-2 grid grid-cols-5">{TABS.map(renderTab)}</div>
-                <div className="flex justify-end gap-4">
-                    <button
-                        onClick={() => handleChartTypeChange('line')}
-                        className={classNames(
-                            'text-white bg-[#17191E] cursor-pointer text-xs rounded-lg p-2 font-semibold shadow-md',
-                            { 'text-[#2962FF]': chartType === 'line' }
-                        )}
-                    >
-                        Line Chart
-                    </button>
-                    <button
-                        onClick={() => handleChartTypeChange('area')}
-                        className={classNames(
-                            'text-white bg-[#17191E] cursor-pointer text-xs rounded-lg p-2 font-semibold shadow-md',
-                            { 'text-[#2962FF]': chartType === 'area' }
-                        )}
-                    >
-                        Area Chart
-                    </button>
-                    <button
-                        onClick={togglePieChart}
-                        className={classNames(
-                            'text-white bg-[#17191E] cursor-pointer text-xs rounded-lg p-2 font-semibold shadow-md',
-                            { 'text-[#2962FF]': showPieChart }
-                        )}
-                    >
-                        Pie Chart
-                    </button>
-                </div>
-            </div>
-            <div className="h-full w-full">
-                {showPieChart ? (
-                    <Chart options={pieChartOptions} series={[tvl, 21_000_000 - tvl]} type="pie" width="100%" height="100%" />
-                ) : (
-                    <Chart options={chartOptions[chartType]} series={[{ data: [] }]} type={chartType} height={height} ref={chartRef} />
-                )}
-            </div>
-        </div>
-    );
+	const ref = useRef()
+
+	useEffect(() => {
+		const groups = groupData(data, selectedTab)
+
+		const parsedData = Object.keys(groups)
+			.map((key) => {
+				const values = groups[key].map((e) => parseInt(e.sum))
+				const maxHeight = Math.max(...groups[key].map((e) => e.height))
+				const lastUnlock = unlockData
+					.filter((e) => e.height <= maxHeight)
+					.reduce((a, e) => a + parseInt(e.sats), 0)
+
+				return {
+					time: groups[key][0].time,
+					value: (Math.max(...values) - parseInt(lastUnlock)) / 1e8
+				}
+			})
+			.sort((a, b) => a?.time - b?.time)
+
+		const volumeData = Object.keys(groups)
+			.map((key) => {
+				const values = groups[key].map((e) => parseInt(e.sum))
+				const heights = groups[key].map((e) => e.height)
+				const maxHeight = Math.max(...heights)
+				const minHeight = Math.min(...heights)
+				const unlocks = unlockData.filter((e) => e.height >= minHeight && e.height <= maxHeight)
+
+				const lockVolume = groups[key].map((e) => parseInt(e.sats)).reduce((a, e) => a + e, 0)
+				const unlockVolume = unlocks.map((e) => parseInt(e.sats)).reduce((a, e) => a + e, 0)
+
+				return {
+					time: groups[key][0].time,
+					value: (lockVolume + unlockVolume) / 1e8,
+					color: lockVolume > unlockVolume ? 'rgba(0, 150, 136, 0.8)' : 'rgba(255,82,82, 0.8)'
+				}
+			})
+			.sort((a, b) => a?.time - b?.time)
+
+		const colors = {
+			backgroundColor: 'black',
+			lineColor: '#2962FF',
+			textColor: 'white',
+			areaTopColor: '#2962FF',
+			areaBottomColor: 'rgba(41, 98, 255, 0.28)'
+		}
+		const handleResize = () => {
+			// @ts-ignore
+			chart.applyOptions({ width: ref.current.clientWidth })
+		}
+
+		const chart = createChart(ref.current, {
+			layout: {
+				background: { type: ColorType.Solid, color: colors.backgroundColor },
+				textColor: colors.textColor
+			},
+			grid: {
+				vertLines: {
+					visible: false
+				},
+				horzLines: {
+					visible: false
+				}
+			},
+			rightPriceScale: {
+				borderVisible: false,
+				textColor: 'black'
+			},
+			timeScale: {
+				borderVisible: false,
+				timeVisible: true
+			},
+			//localization: {
+			//priceFormatter: (p) => {
+			//return `${p} BSV`
+			//}
+			//},
+
+			// @ts-ignore
+			width: ref.current.clientWidth,
+			height: height
+		})
+		chart.timeScale().fitContent()
+
+		// Locks
+		const newSeries = chart.addAreaSeries({
+			lineColor: '#34D399',
+			topColor: 'rgba(52, 211, 153, 0.9)',
+			bottomColor: 'rgba(161, 255, 139, 0.04)'
+		})
+		newSeries.setData(parsedData)
+		//newSeries.setMarkers([
+			//{
+				//time: '2024-02-07',
+				//position: 'inBar',
+				//color: 'white',
+				//shape: 'circle',
+				//text: 'yours.org launch'
+			//}
+		//])
+
+		// Lock Volume
+		const volumeSeries = chart.addHistogramSeries({
+			color: '#26a69a',
+			priceFormat: {
+				type: 'volume'
+			},
+			priceScaleId: ''
+		})
+		volumeSeries.setData(volumeData)
+
+		window.addEventListener('resize', handleResize)
+
+		return () => {
+			window.removeEventListener('resize', handleResize)
+			chart.remove()
+		}
+	}, [data, height, unlockData, selectedTab])
+
+	const renderTab = React.useCallback(
+		(e) => {
+			return (
+				<div
+					className={classNames(
+						'flex justify-center bg-[#17191E] cursor-pointer text-xs rounded-lg p-2 font-semibold',
+						{
+							['text-white']: selectedTab === e,
+							['text-[#D0D5DD]']: selectedTab !== e
+						}
+					)}
+					key={e}
+					onClick={() => setSelectedTab(e)}
+				>
+					{e}
+				</div>
+			)
+		},
+		[selectedTab]
+	)
+
+	return (
+		<div className="h-full w-full relative">
+			<div className="md:absolute l-0 t-0 z-10 flex flex-col px-4 gap-2">
+				<p className="text-sm font-semibold text-white">Locked Coins</p>
+				<div className="bg-[#17191E] rounded-lg p-4 flex justify-between gap-8 md:w-[350px] max-w-full">
+					<div className="flex flex-col">
+						<div className="flex gap-2 items-center">
+							<img src="/bsv.svg" className="h-4 w-4" />
+							<p className="text-2xl text-white whitespace-nowrap">
+								{formatNumber(tvl.toFixed(2))}
+							</p>
+						</div>
+						<p
+							className={classNames('text-sm whitespace-nowrap', {
+								['text-[#6CE9A6]']: percentChange > 0,
+								['text-red-500']: percentChange < 0
+							})}
+						>
+							{formatNumber(((dayAgoTvl * percentChange) / 100).toFixed(2))} (
+							{percentChange.toFixed(2)}%)
+						</p>
+					</div>
+					<div className="flex flex-col">
+						<p className="text-2xl text-white whitespace-nowrap">
+							${formatNumber((tvl * exchangeRate).toFixed(2))}
+						</p>
+						<p
+							className={classNames('text-right text-sm whitespace-nowrap', {
+								['text-[#6CE9A6]']: percentChange > 0,
+								['text-red-500']: percentChange < 0
+							})}
+						>
+							${formatNumber((((dayAgoTvl * percentChange) / 100) * exchangeRate).toFixed(2))}
+						</p>
+					</div>
+				</div>
+				<div className="gap-2 grid grid-cols-5">{TABS.map(renderTab)}</div>
+			</div>
+			<div className="h-full w-full" ref={ref} />
+		</div>
+	)
 }
-
-const pieChartOptions = {
-    labels: ['Locked', 'Unlocked'],
-    colors: ['#00FF00', '#FF1744']
-};
-
-const chartOptions = {
-    line: {
-        xaxis: {
-            type: 'datetime',
-            labels: {
-                style: {
-                    colors: '#BDBDBD',
-                    fontSize: '12px'
-                }
-            }
-        },
-        yaxis: {
-            labels: {
-                style: {
-                    colors: '#BDBDBD',
-                    fontSize: '12px'
-                },
-                formatter: function (val) {
-                    return formatNumber(val);
-                }
-            },
-            title: {
-                text: 'Locked Sats'
-            }
-        },
-        grid: {
-            borderColor: '#303030',
-            strokeDashArray: 4,
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            width: 1,
-        },
-        colors: ['#00E676', '#FF1744'],
-        tooltip: {
-            enabled: true,
-            x: {
-                show: true,
-                format: 'dd MMM yyyy'
-            },
-            y: {
-                formatter: function (val) {
-                    return formatNumber(val);
-                }
-            }
-        },
-        markers: {
-            size: 0
-        }
-    },
-    area: {
-        xaxis: {
-            type: 'datetime',
-            labels: {
-                style: {
-                    colors: '#BDBDBD',
-                    fontSize: '12px'
-                }
-            }
-        },
-        yaxis: {
-            labels: {
-                style: {
-                    colors: '#BDBDBD',
-                    fontSize: '12px'
-                },
-                formatter: function (val) {
-                    return formatNumber(val);
-                }
-            },
-            title: {
-                text: 'Locked Sats'
-            }
-        },
-        grid: {
-            borderColor: '#303030',
-            strokeDashArray: 4,
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            width: 1,
-        },
-        colors: ['#00E676'], // Set the color to green
-        tooltip: {
-            enabled: true,
-            x: {
-                show: true,
-                format: 'dd MMM yyyy'
-            },
-            y: {
-                formatter: function (val) {
-                    return formatNumber(val);
-                }
-            }
-        },
-        markers: {
-            size: 0
-        },
-        fill: {
-            type: 'gradient',
-            gradient: {
-                shadeIntensity: 1,
-                opacityFrom: 0.7,
-                opacityTo: 0.9,
-                stops: [0, 100]
-            }
-        }
-    }
-};
