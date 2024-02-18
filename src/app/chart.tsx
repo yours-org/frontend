adea2e1f32248e07ce5991e53d2864343402eaf0
NlockTime <159491556+NlockTime@users.noreply.github.com>
2024-02-18 13:07:27 -0800

    Update chart.tsx

--- src/app/chart.tsx
+++ src/app/chart.tsx
@@ -260,7 +260,16 @@ export default function Chart(props: {
 						</p>
 					</div>
 				</div>
-				
+				<div className="flex flex-col bg-[#17191E] rounded-lg p-2">
+					<p className="text-sm flex justify-between items-center bg-[#17191E] rounded-lg p-2">
+						<span className="text-gray-500">Percent of circulating supply locked</span>
+						<span>
+							<span className="text-white">
+								{((tvl / totalCirculatingSupply) * 100).toFixed(2)}%
+							</span>
+						</span>
+					</p>
+				</div>
 				<div className="gap-2 grid grid-cols-5">{TABS.map(renderTab)}</div>
 			</div>
 			<div className="h-full w-full" ref={ref} />
