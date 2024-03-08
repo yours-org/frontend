5d2f8a26b463f855dedd737749279779c006004d
Randy <48290359+rxxndy@users.noreply.github.com>
2024-03-08 11:53:36 -0600

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,7 +20,7 @@ const buttonVariants = cva(
 			size: {
 				default: 'h-10 px-4 py-2',
 				sm: 'h-9 rounded-md px-3',
-				lg: 'h-11 rounded-full px-8 font-bold text-xl',
+				lg: 'h-11 rounded-full px-8 font-bold text-base',
 				icon: 'h-10 w-10'
 			}
 		},
